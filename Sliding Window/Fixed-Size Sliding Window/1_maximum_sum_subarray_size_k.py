d8ffe1196e2b0dff3727b43a43ed211970a950c8
Anupam Debnath <65649115+anupam6335@users.noreply.github.com>
2025-09-07 17:32:16 +0530

    Added brute force and optimize solution

--- Sliding Window/Fixed-Size Sliding Window/1_maximum_sum_subarray_size_k.py
+++ Sliding Window/Fixed-Size Sliding Window/1_maximum_sum_subarray_size_k.py
@@ -1,4 +1,20 @@
 #  Question Link : https://www.geeksforgeeks.org/problems/max-sum-subarray-of-size-k5313/1
+
+# brute force solution 
+def maximum_sum_subarray_size_k(arr, k):
+    n = len(arr)
+    currSum = 0
+    maxSum = float('-inf')
+    for i in range(0, n - k+1):
+        currSum += arr[i]
+        for j in range(i + 1, i + k):
+            currSum += arr[j]
+        
+        maxSum = max(maxSum, currSum)
+        currSum = 0
+    return maxSum if maxSum != float('-inf') else -1
+    
+# Optimize Solution 
 def maximum_sum_subarray_size_k(arr, k):
     n = len(arr)
     currSum = 0
@@ -15,5 +31,6 @@ def maximum_sum_subarray_size_k(arr, k):
             i += 1
 
     return maxSum if maxSum != float('-inf') else -1    
+
 
 print(maximum_sum_subarray_size_k([100, 200, 300, 400, 0], 2))
